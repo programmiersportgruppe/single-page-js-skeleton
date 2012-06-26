daca3d50582602fdc649529a94f4f870f0acf0cd
Matt <matt@matthias.(none)>
2012-06-26 18:20:49 +0200

    barebones sammy routing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeleton::Application.config.secret_token = 'f9603dd7b8666e133df2acfc4de260ab501f578972254740871f42d5b8b7ab03ca18e99e90a34feabd005e8379a136dfd50e15e7f1b1373ec230bd342e972bcb'
