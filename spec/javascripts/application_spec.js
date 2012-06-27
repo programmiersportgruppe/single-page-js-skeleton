abe7d0b632e211c353b71f5ec9ea0341c3cba17b
Matt <matt@matthias.(none)>
2012-06-27 14:17:01 +0200

    added a manual jasmine runner

---
+++ spec/javascripts/application_spec.js
@@ -0,0 +1,16 @@
+describe("application", function() {
+    it("should toggle the visiblity of components", function() {
+        application = Application()
+
+        expect(application.todos.visible()).toEqual(false);
+        expect(application.notes.visible()).toEqual(false);
+        application.showTodos();
+
+        expect(application.todos.visible()).toEqual(true);
+        expect(application.notes.visible()).toEqual(false);
+        application.showNotes();
+
+        expect(application.todos.visible()).toEqual(false);
+        expect(application.notes.visible()).toEqual(true);
+    });
+});
