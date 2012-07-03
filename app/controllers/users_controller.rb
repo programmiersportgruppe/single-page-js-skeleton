6edeb395cf323b44cdfb7cdc42c68b24508060c4
Matt <matt@matthias.(none)>
2012-07-03 15:34:53 +0200

    fixed signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(params)
+    @user.username = params['username']
     if @user.save
       @message = "You are now signed up!"
       render 'application/success'
