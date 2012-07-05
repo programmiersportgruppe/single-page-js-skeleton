9d8b2e6c788dccdef2d41ad4d87d227d140a38aa
Matt <matt@matthias.(none)>
2012-07-05 12:06:36 +0200

    now hiding components until they are parsed by ko

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,10 @@
-require(["application", "notifier", "router", "knockout", "custom-ko"], function (Application, Notifier, Router, ko) {
+require(["application", "notifier", "router", "knockout", "jquery", "custom-ko"], function (Application, Notifier, Router, ko, $) {
 
   application = Application(Notifier());
 
   ko.applyBindings(application);
+
+  $(".initial_component").removeClass("initial_component");
 
   Router(application).run();
 
